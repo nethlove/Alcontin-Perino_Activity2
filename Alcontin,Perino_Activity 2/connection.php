a5360d18a59e61d79634c2f8204b848f174e8099
Kenneth Love F. Alcontin <kennethalcontin07@gmail.com>
2025-10-23 12:36:25 +0800

    update

---
+++ Alcontin,Perino_Activity 2/connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $connect = new PDO("mysql:host=$servername;dbname=alcontin_database", $username, $password);
+  // set the PDO error mode to exception
+  $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
